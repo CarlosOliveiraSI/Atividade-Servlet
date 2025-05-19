dc57b19cbb7c7c00fe684df013aac9f8b7b28c9e
Carlos Oliveira <143031261+CarlosOliveiraSI@users.noreply.github.com>
2025-05-18 23:38:03 -0300

    Add files via upload

--- ServletEst/src/main/java/Controller/HeadersServlet.java
+++ ServletEst/src/main/java/Controller/HeadersServlet.java
@@ -6,11 +6,15 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import com.sun.net.httpserver.Request;
 
 /**
  * Servlet implementation class HeadersServlet
  */
-@WebServlet("/HeadersServlet")
+@WebServlet("/headers")
 public class HeadersServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -27,15 +31,38 @@ public class HeadersServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html;charset=UTF-8");
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h2>Análise de Cabeçalhos</h2>");
+		out.println("ul");
+		
+		Enumeration<String> headesNames = request.getHeaderNames();
+		
+		while (headesNames.hasMoreElements()) {
+			String name = headesNames.nextElement();
+			String valor = request.getHeader(name);
+			out.println("<li><strong>"+ name +"</strong>: " + valor + "</li>");
+			
+			String ip = request.getRemoteAddr();
+			out.println("<li><strong> IP do usuário</strong>:" + ip + "</li>");
+			
+			String protocolo = request.getProtocol();
+			out.println("<li><strong>Versão do HTTP</strong>:" + protocolo +"</li>");
+		}
+			
+		
+		out.println("</ul>");
+		out.println("</body>");
+		out.println("</html>");
+		
+		
+		
+		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
 
 }
